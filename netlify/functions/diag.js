1ebf7d7e916b58b3cb9e3e350ff465212f905421
Vedant Kankate <98166840+Vedant2402@users.noreply.github.com>
2025-08-11 20:51:06 -0700

    fixes

--- netlify/functions/diag.js
+++
@@ -1,8 +0,0 @@
-// Diagnostics Netlify Function
-export async function handler() {
-  const apiKey = process.env.OPENAI_API_KEY;
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ hasKey: Boolean(apiKey), keyLength: apiKey ? apiKey.length : 0, runtime: process.version })
-  };
-}
